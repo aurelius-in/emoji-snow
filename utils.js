cb1fe0d929e643d9d2f1c5a232e2673b17bb3871
Oliver Ellison, MS SD <89275354+aurelius-in@users.noreply.github.com>
2023-10-30 23:45:20 -0500

    made ctx accessible by draw

--- utils.js
+++ utils.js
@@ -1,6 +1,5 @@
-    const canvas = document.getElementById("gameCanvas");
-    const ctx = canvas.getContext("2d");
-
+    let canvas = document.getElementById("gameCanvas");
+    let ctx = canvas.getContext("2d");
     let gameState = 'selectCharacter', playerEmoji = null, scrollX = 0, scrollY = 0;
     let player = { x: 50, y: 960, width: 50, height: 50, vx: 10, vy: 0, gravity: 0.5, jump: -10, grounded: false, jumping: false };
     let hills = [{x: 0, y: 960}, {x: 500, y: 960}];
