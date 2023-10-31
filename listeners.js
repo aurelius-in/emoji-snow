65166130b416a082b7505e8bf68992eb59e46dfa
Oliver Ellison, MS SD <89275354+aurelius-in@users.noreply.github.com>
2023-10-30 23:34:31 -0500

    added mouse event listeners

--- listeners.js
+++ listeners.js
@@ -44,3 +44,60 @@ canvas.addEventListener("touchend", function(e) {
     }
   }
 });
+
+
+// Event listeners for mouse clicks
+canvas.addEventListener("mousedown", function(e) {
+  e.preventDefault();
+  const mouseX = e.clientX;
+  const mouseY = e.clientY;
+
+  // Character selection logic
+  if (gameState === 'selectCharacter') {
+    if (mouseX >= 600 && mouseX <= 900 && mouseY >= 400 && mouseY <= 700) {
+      playerEmoji = "🏂";
+      gameState = 'play';
+    } else if (mouseX >= 1400 && mouseX <= 1700 && mouseY >= 400 && mouseY <= 700) {
+      playerEmoji = "⛷️";
+      gameState = 'play';
+    }
+  } else {
+    // In-game logic to set the initial point for detecting speed up or slow down
+    touchStartX = mouseX;
+    touchStartY = mouseY;
+  }
+});
+
+canvas.addEventListener("mouseup", function(e) {
+  e.preventDefault();
+  const mouseX = e.clientX;
+  const mouseY = e.clientY;
+
+  // In-game movement logic
+  if (gameState !== 'selectCharacter') {
+    touchEndX = mouseX;
+    touchEndY = mouseY;
+
+    const deltaX = touchEndX - touchStartX;
+    const deltaY = touchEndY - touchStartY;
+
+    if (Math.abs(deltaX) > Math.abs(deltaY)) {
+      if (deltaX > 0) {
+        // Speed up
+        playerMomentum = 50;
+        lastSwipeTime = new Date().getTime();
+        player.x += 50;
+      } else {
+        // Slow down
+        player.x -= 50;
+      }
+    } else {
+      if (deltaY < 0 && player.grounded) {
+        // Jump
+        player.vy = player.jump;
+        player.grounded = false;
+        player.jumping = true;
+      }
+    }
+  }
+});
